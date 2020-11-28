4e2f94c4b1d54e8421315a5ac06bb51701a3bfef
kjcaway <whdckstngh@naver.com>
2020-11-28 17:18:50 +0900

    ** 코드 정리

--- src/main/kotlin/com/example/demo/api/redistest/repository/MemberRepositoryRedis.kt
+++ src/main/kotlin/com/example/demo/api/redistest/repository/MemberRepositoryRedis.kt
@@ -5,5 +5,4 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface MemberRepositoryRedis : CrudRepository<MemberRedis, String>{
-}
+interface MemberRepositoryRedis : CrudRepository<MemberRedis, String>
